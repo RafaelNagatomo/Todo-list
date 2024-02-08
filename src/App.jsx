0a98f3711b7dbb90b58b7903a01722bc7d04fbb7
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2024-02-08 11:31:34 -0300

    delete array

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,48 @@
-import { useState } from "react"
-import Todo from './components/Todo'
-import "./App.css"
-import TodoForm from "./components/TodoForm"
-import Search from "./components/Search"
-import Filter from "./components/Filter"
+import { useState } from "react";
+import Todo from "./components/Todo";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import Search from "./components/Search";
+import Filter from "./components/Filter";
 
 function App() {
-  const [todos, setTodos] = useState([
-  {
-    id:1,
-    text: "Criar funcionalidade x no sistema",
-    category: "Trabalho",
-    isCompleted: false,
-  },
-  {
-    id:2,
-    text: "Ir para a academia",
-    category: "Pessoal",
-    isCompleted: false,
-  },
-  {
-    id:3,
-    text: "Estudar React",
-    category: "Estudos",
-    isCompleted: false,
-  },
-  ])
+  const [todos, setTodos] = useState([]);
 
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
 
-  const [filter, setFilter] = useState("All")
+  const [filter, setFilter] = useState("All");
 
-  const [sort, setSort] = useState("Asc")
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
-
     const newTodos = [
-      ...todos, 
+      ...todos,
       {
         id: Math.floor(Math.random() * 10000),
         text,
         category,
         isCompleted: false,
       },
-    ]
+    ];
 
-    setTodos(newTodos)
-  }
+    setTodos(newTodos);
+  };
 
   const removeTodo = (id) => {
-    const newTodos = [...todos]
-    const filteredTodos = newTodos.filter(todo => 
-      todo.id !== id ? todo : null)
-      setTodos(filteredTodos)
-  }
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+  };
 
   const completeTodo = (id) => {
-    const newTodos = [...todos]
-    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
-    setTodos(newTodos)
-  }
+    const newTodos = [...todos];
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+    );
+    setTodos(newTodos);
+  };
 
   return (
     <div className="app">
@@ -67,19 +50,35 @@ function App() {
       <Search search={search} setSearch={setSearch} />
       <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
-      <h2>Tarefas:</h2>
+        <h2>Tarefas:</h2>
         {todos
-        .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
-        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
-        .sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text))
-        .map((todo) => (
-          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
-        ))}
-       
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
       </div>
       <TodoForm addTodo={addTodo} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
